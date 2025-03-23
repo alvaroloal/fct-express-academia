66f5ecc0f972cc2ed4bdffbb53901ea52907e27a
alvaroloal <alorentealman@gmail.com>
2025-03-23 12:28:21 +0100

    conexión a bbdd mysql

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+import estudiantesRoutes from './routes/estudiantesRoutes';
+import profesoresRoutes from './routes/profesoresRoutes';
+import cursosRoutes from './routes/cursosRoutes';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    console.log('Hola mundo');
+    res.send('Hola mundo');
+});
+
+
+app.use('/estudiantes', estudiantesRoutes);
+app.use('/profesores', profesoresRoutes);
+app.use('/cursos', cursosRoutes);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-
-import estudiantesRoutes from'./routes/estudiantesRoutes';
-import profesoresRoutes from'./routes/profesoresRoutes';
-import cursosRoutes from'./routes/cursosRoutes';
-
+import app from './app';
 const port = 3005;
-const app = express();
+import { AppDataSource } from './db/connection';
 
-app.use(morgan('dev'));
-app.use(cors());
+async function main() {
+    try {
+        await AppDataSource.initialize();
+        console.log("Base de datos conectada")
+        app.listen(port, () => {
+            console.log(`Servidor activo en el puerto: ${port}`);
+        });
+    } catch (err) {
+        if (err instanceof Error) {
+            console.log(err.message);
+        }
+    }
+}
 
-app.get('/', (req, res) => {
-    console.log('Hola mundo');
-    res.send('Hola mundo');
-});
-
-app.listen(port, () => {
-    console.log(`Servidor activo en el puerto: ${port}`);
-});
-
-app.use('/estudiantes', estudiantesRoutes);
-app.use('/profesores', profesoresRoutes);
-app.use('/cursos', cursosRoutes);
+main();
